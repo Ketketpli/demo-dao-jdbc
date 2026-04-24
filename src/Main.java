0e2406c7bc4362c77cd3b1d7cdc9127dc4417e3e
Paulo Henrique <varelapaulo852@gmail.com>
2026-04-24 16:15:14 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         Department obj = new Department(1, "Books");
 
         System.out.println(obj);
+
+        Seller seller = new Seller(123, "Don", "don@gmail.com", new Date(), 2000.00, obj);
+
+        System.out.println(seller);
     }
 }
