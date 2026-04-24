ad28d4c2eeebce3753f11d2bcee140ffc9588147
Paulo Henrique <varelapaulo852@gmail.com>
2026-04-24 17:06:24 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        Department obj = new Department(1, "Books");
-
-        System.out.println(obj);
-
-        Seller seller = new Seller(123, "Don", "don@gmail.com", new Date(), 2000.00, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
