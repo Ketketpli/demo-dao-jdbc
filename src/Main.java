f895d59624b2c1317abf3ad108234435e57406b9
Paulo Henrique <varelapaulo852@gmail.com>
2026-04-27 17:16:40 -0300

    update implementation

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println(seller);
 
         System.out.println("\n==== TEST 2: seller findByDepartment====");
-        Department department = new Department(2, null);
+        Department department = new Department(4, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list) {
             System.out.println(obj);
@@ -28,9 +28,16 @@ public class Main {
             System.out.println(obj);
         }
 
-        System.out.println("\n==== TEST 3: seller findAll====");
+        System.out.println("\n==== TEST 4: seller insert====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n==== TEST 5: seller update====");
+        seller = sellerDao.findById(1);
+        seller.setEmail("martha1@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
